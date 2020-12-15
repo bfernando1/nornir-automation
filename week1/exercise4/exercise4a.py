01b905ae028a6e484ac66757570c436f38c7c0ee
bfernando1 <bradleylasnames@gmail.com>
2020-12-14 22:08:26 -0800

    Add descriptions to exercises

--- week1/exercise4/exercise4a.py
+++ week1/exercise4/exercise4a.py
@@ -1,11 +1,19 @@
 #! /usr/bin/env python3
+"""
+Author: Bradley Fernando
+Purpose: Outputs a string for each item in hosts using Nornir run
 
+Usage: 
+    python exercise4a.py
+
+Output:
+    Hello
+    Hello
+"""
 from nornir import InitNornir
 
 def my_task(task):
-    print()
     print("Hello")
-    print()
 
 nr = InitNornir()
 nr.run(task=my_task)
--- week1/exercise4/exercise4b.py
+++ week1/exercise4/exercise4b.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python3
+"""
+Author: Bradley Fernando
+Purpose: Calls a Nornir task to print a string and the hostname included 
+         in hosts group.  
+
+Usage: 
+    python exercise4b.py
+
+Output:
+        
+    It's raining in San Francisco
+    hostname: rtr1.sjc
+    
+    
+    It's raining in San Francisco
+    hostname: rtr2.sjc
+    
+"""
 
 from nornir import InitNornir
 
