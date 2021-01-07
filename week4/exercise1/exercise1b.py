b199a50b401f335adc5a3d32dc482d73c5eb7de5
bfernando1 <bradleylasnames@gmail.com>
2021-01-06 21:39:28 -0800

    First iteration of exercise1b

--- week4/exercise1/exercise1b.py
+++ week4/exercise1/exercise1b.py
@@ -12,8 +12,8 @@ WEEK_SECONDS = 7 * DAY_SECONDS
 YEAR_SECONDS = 365 * DAY_SECONDS
 
 nr = InitNornir(config_file="../../config.yaml")
-ios_filt = F(groups__contains="ios")
-nr = nr.filter(ios_filt)
+#junos_filt = F(groups__contains="junos")
+#nr = nr.filter(junos_filt)
 
 def get_uptime(task):
     all_cmds = {
@@ -84,19 +84,65 @@ def parse_uptime_nxos(uptime_str):
         + (seconds)
     )
     return uptime_sec
+
+def parse_uptime_eos(uptime_str):
+    """
+    Extract the uptime string from the given Arista Device.
+
+    Return the uptime in seconds as an integer.
+    """
+    # Initialize to zero
+    (weeks, days, hours, minutes) = (0, 0, 0, 0)
+    uptime_str = uptime_str.strip()
+    time_list = uptime_str.split(",")
+    for element in time_list:
+        if re.search("weeks", element):
+            weeks = int(element.split()[0])         
+        elif re.search("days", element):
+            days  = int(element.split()[0])         
+        elif re.search("hour", element):
+            hours = int(element.split()[0])         
+        elif re.search("minute", element):
+            minutes = int(element.split()[0])         
+        
+    uptime_sec = (
+        (weeks * WEEK_SECONDS)
+        + (days * DAY_SECONDS)
+        + (hours * 3600)
+        + (minutes * 60)
+    )
+    return uptime_sec
+
        
 print("\nGathering uptime from all devices...\n")
-agg_result = nr.run(task=get_uptime, num_workers=1)
+agg_result = nr.run(task=get_uptime)
+print("{:>10} {:>10}".format("Device", "Uptime (sec)"))
+print(('-' * 10 + ' ') * 2)
+
+
 for host, line in agg_result.items():
     raw_uptime = line[1].result
     # Uptime string for IOS devices
     if " uptime is " in raw_uptime and "Kernal" not in raw_uptime: 
         _, uptime_str = raw_uptime.split(" uptime is ")
         uptime = parse_uptime_ios(uptime_str)
-        print(f"Host: {host}, Uptime (s): {uptime}")
+        print("{:>10} {:>10}".format(host, uptime))
+
     # Uptime string in NXOS devices
     elif " uptime is " in raw_uptime and "Kernal" in raw_uptime: 
         _, uptime_str = raw_uptime.split(" uptime is ")
         uptime = parse_uptime_nxos(uptime_str)
-        print(f"Host: {host}, Uptime (s): {uptime}")
+        print("{:>10} {:>10}".format(host, uptime))
 
+    # Uptime string in EOS devices
+    elif "Uptime" in raw_uptime:
+        _, uptime_str = raw_uptime.split("Uptime: ")
+        uptime = parse_uptime_eos(uptime_str)
+        print("{:>10} {:>10}".format(host, uptime))
+
+    # Artifically reboot JUNOS devices
+    elif "System booted" in raw_uptime:
+        uptime = 90
+    
+    if uptime < DAY_SECONDS:
+        print(f"\nWARNING! {host} rebooted {uptime} second(s) ago\n")
