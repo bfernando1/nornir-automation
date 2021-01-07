7f5bb40c9dec0b5ae31c89a4221bf85fe35871a6
bfernando1 <bradleylasnames@gmail.com>
2021-01-06 18:00:54 -0800

    Add uptime in seconds exercise

--- week4/exercise1/exercise1a.py
+++ week4/exercise1/exercise1a.py
@@ -1,3 +1,51 @@
+"""
+Author: Bradley Fernando
+Purpose: Gathers the uptime from all devices using a custom Nornir task.
+
+Usage:
+    python exercise1a.py
+
+Output:
+
+    Gathering uptime from all devices...
+    
+    Working on cisco3:
+    ------------------------------------------------------------
+    cisco3 uptime is 23 weeks, 4 days, 6 hours, 21 minutes
+    
+    Working on cisco4:
+    ------------------------------------------------------------
+    cisco4 uptime is 31 weeks, 5 days, 6 hours, 38 minutes
+    
+    Working on nxos1:
+    ------------------------------------------------------------
+    Kernel uptime is 142 day(s), 7 hour(s), 3 minute(s), 6 second(s)
+    
+    Working on nxos2:
+    ------------------------------------------------------------
+    Kernel uptime is 142 day(s), 6 hour(s), 54 minute(s), 40 second(s)
+    
+    Working on arista1:
+    ------------------------------------------------------------
+    Uptime:                 91 weeks, 4 days, 22 hours and 2 minutes
+    
+    Working on arista2:
+    ------------------------------------------------------------
+    Uptime:                 67 weeks, 6 days, 5 hours and 43 minutes
+    
+    Working on arista3:
+    ------------------------------------------------------------
+    Uptime:                 91 weeks, 4 days, 21 hours and 43 minutes
+    
+    Working on arista4:
+    ------------------------------------------------------------
+    Uptime:                 91 weeks, 4 days, 21 hours and 59 minutes
+    
+    Working on srx2:
+    ------------------------------------------------------------
+    System booted: 2020-06-18 16:18:53 PDT (28w6d 00:19 ago)
+
+"""
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
---
+++ week4/exercise1/exercise1b.py
@@ -0,0 +1,102 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+from nornir.core.task import Result
+from nornir.core.filter import F
+import re
+
+
+HOURS_SECONDS = 3600
+DAY_SECONDS = 24 * HOURS_SECONDS
+WEEK_SECONDS = 7 * DAY_SECONDS
+YEAR_SECONDS = 365 * DAY_SECONDS
+
+nr = InitNornir(config_file="../../config.yaml")
+ios_filt = F(groups__contains="ios")
+nr = nr.filter(ios_filt)
+
+def get_uptime(task):
+    all_cmds = {
+        "eos": "show version | inc Uptime",
+        "ios": "show version | inc uptime",
+        "junos": "show system uptime | match System",
+        "nxos": "show version | inc uptime"
+    }
+    cmd = all_cmds[task.host.platform] 
+    task.run(task=netmiko_send_command, command_string=cmd)
+
+def parse_uptime_ios(uptime_str):
+    """
+    Extract the uptime string from the given Cisco IOS Device.
+
+    Return the uptime in seconds as an integer.
+    """
+    # Initialize to zero
+    (years, weeks, days, hours, minutes) = (0, 0, 0, 0, 0)
+    uptime_str = uptime_str.strip()
+    time_list = uptime_str.split(",")
+    for element in time_list:
+        if re.search("year", element):
+            years = int(element.split()[0])         
+        elif re.search("week", element):
+            weeks = int(element.split()[0])         
+        elif re.search("day", element):
+            days = int(element.split()[0])         
+        elif re.search("hour", element):
+            hours = int(element.split()[0])         
+        elif re.search("minute", element):
+            minutes = int(element.split()[0])         
+        
+    uptime_sec = (
+        (years * YEAR_SECONDS)
+        + (weeks * WEEK_SECONDS)
+        + (days * DAY_SECONDS)
+        + (hours * 3600)
+        + (minutes * 60)
+    )
+    return uptime_sec
+
+ 
+def parse_uptime_nxos(uptime_str):
+    """
+    Extract the uptime string from the given NX-OS Device.
+
+    Return the uptime in seconds as an integer.
+    """
+    # Initialize to zero
+    (days, hours, minutes, seconds) = (0, 0, 0, 0, 0)
+    uptime_str = uptime_str.strip()
+    time_list = uptime_str.split(",")
+    for element in time_list:
+        if re.search("day", element):
+            days = int(element.split()[0])         
+        elif re.search("hour", element):
+            hours = int(element.split()[0])         
+        elif re.search("minute", element):
+            minutes = int(element.split()[0])         
+        elif re.search("second", element):
+            seconds  = int(element.split()[0])         
+        
+    uptime_sec = (
+        (days * DAY_SECONDS)
+        + (hours * 3600)
+        + (minutes * 60)
+        + (seconds)
+    )
+    return uptime_sec
+       
+print("\nGathering uptime from all devices...\n")
+agg_result = nr.run(task=get_uptime, num_workers=1)
+for host, line in agg_result.items():
+    raw_uptime = line[1].result
+    # Uptime string for IOS devices
+    if " uptime is " in raw_uptime and "Kernal" not in raw_uptime: 
+        _, uptime_str = raw_uptime.split(" uptime is ")
+        uptime = parse_uptime_ios(uptime_str)
+        print(f"Host: {host}, Uptime (s): {uptime}")
+    # Uptime string in NXOS devices
+    elif " uptime is " in raw_uptime and "Kernal" in raw_uptime: 
+        _, uptime_str = raw_uptime.split(" uptime is ")
+        uptime = parse_uptime_nxos(uptime_str)
+        print(f"Host: {host}, Uptime (s): {uptime}")
+
