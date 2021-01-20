c2c9c2996b0acfcfd858da06a8ad7b27c561470b
bfernando1 <bradleylasnames@gmail.com>
2021-01-19 22:02:13 -0800

    Complete rendered configs

--- week5/exercise5/exercise5b.py
+++ week5/exercise5/exercise5b.py
@@ -1,22 +1,31 @@
 from nornir import InitNornir
 from nornir.plugins.tasks import data, text
+from nornir.core.filter import F
 import ipdb
 
 def render_config(task):
     config_path = f"{task.host.platform}/"
+
+    # Interface config
     int_config = task.run(
         task=text.template_file, template="interface.j2", path=config_path, **task.host
     )
-#    bgp_config = task.run(
-#        task=text.template_file, template="bgp.j2", path=config_path, **task.host
-#    )
-    ipdb.set_trace() 
+    int_config = int_config[0].result
+    task.host['int_rendered_config'] = int_config
 
+    # BGP config
+    bgp_config = task.run(
+        task=text.template_file, template="bgp.j2", path=config_path, **task.host
+    )
+    bgp_config = bgp_config[0].result
+    task.host['bgp_rendered_config'] = bgp_config
+    
 def main():
     nr = InitNornir(config_file="config.yaml")
-    nr = nr.filter(name="nxos1")
+    nxos_filt = F(groups__contains="nxos")
+    nr = nr.filter(nxos_filt)
     agg_result = nr.run(task=render_config)    
-    #ipdb.set_trace() 
+    ipdb.set_trace() 
 
 if __name__ == "__main__":
     main()
