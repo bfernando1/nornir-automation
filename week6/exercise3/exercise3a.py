327bbdddc5213429c3b4df6ab3877f439a9bd808
bfernando1 <bradleylasnames@gmail.com>
2021-01-29 08:45:15 -0800

    Include expected output in docstring

--- week6/exercise3/exercise3a.py
+++ week6/exercise3/exercise3a.py
@@ -1,3 +1,38 @@
+"""
+Author: Bradley Fernando
+Purpose: Prompts the user for the device password before executing the task.
+
+Usage:
+    python exercise3a.py
+
+Output:
+    Password:
+    2021-01-29 08:42:22,314 -  nornir.core -     INFO -        run() - Running task 'send_to_device' with args {} on 1 hosts
+    2021-01-29 08:42:22,315 - nornir.core.task -    DEBUG -      start() - Host 'arista1': running task 'send_to_device'
+    2021-01-29 08:42:22,315 - nornir.core.task -    DEBUG -      start() - Host 'arista1': running task 'netmiko_send_command'
+    send_to_device******************************************************************
+    * arista1 ** changed : False ***************************************************
+    vvvv send_to_device ** changed : False vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv INFO
+    ---- netmiko_send_command ** changed : False ----------------------------------- INFO
+    VRF: default
+    Codes: C - connected, S - static, K - kernel,
+           O - OSPF, IA - OSPF inter area, E1 - OSPF external type 1,
+           E2 - OSPF external type 2, N1 - OSPF NSSA external type 1,
+           N2 - OSPF NSSA external type2, B I - iBGP, B E - eBGP,
+           R - RIP, I L1 - IS-IS level 1, I L2 - IS-IS level 2,
+           O3 - OSPFv3, A B - BGP Aggregate, A O - OSPF Summary,
+           NG - Nexthop Group Static Route, V - VXLAN Control Service,
+           DH - DHCP client installed default route, M - Martian
+    
+    Gateway of last resort:
+     S      0.0.0.0/0 [1/0] via 10.220.88.1, Vlan1
+    
+     C      10.220.88.0/24 is directly connected, Vlan1
+    
+    ^^^^ END send_to_device ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"""
+
+
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
--- week6/exercise3/exercise3b.py
+++ week6/exercise3/exercise3b.py
@@ -1,3 +1,21 @@
+"""
+Author: Bradley Fernando
+Purpose: Display custom logging messages in the log file
+
+Usage:
+    python exercise3b.py
+
+Output:
+    Password:
+    2021-01-29 08:43:48,298 -       nornir -    DEBUG -       main() - Flat is better than nested
+    2021-01-29 08:43:48,298 -       nornir - CRITICAL -       main() - Sparse is better than dense
+    2021-01-29 08:43:48,298 -       nornir -    ERROR -       main() - Unless explicitly silenced
+    2021-01-29 08:43:48,299 -  nornir.core -     INFO -        run() - Running task 'send_to_device' with args {} on 1 hosts
+    2021-01-29 08:43:48,299 - nornir.core.task -    DEBUG -      start() - Host 'arista1': running task 'send_to_device'
+    2021-01-29 08:43:48,300 - nornir.core.task -    DEBUG -      start() - Host 'arista1': running task 'netmiko_send_command'
+"""
+
+
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.functions.text import print_result
@@ -8,9 +26,10 @@ import logging
 PASSWORD = getpass.getpass()
 logger = logging.getLogger("nornir")
 
+
 def send_to_device(task):
     cmd = "show ip route"
-    task.host.password = PASSWORD 
+    task.host.password = PASSWORD
     task.run(task=netmiko_send_command, command_string=cmd)
 
 
@@ -20,6 +39,8 @@ def main():
 
     # Add logging test statements
     logger.debug("Flat is better than nested")
+    logger.critical("Sparse is better than dense")
+    logger.error("Unless explicitly silenced")
 
     agg_result = nr.run(task=send_to_device)
     print_result(agg_result)
