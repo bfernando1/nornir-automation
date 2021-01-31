35424363b5d9354a86c2b7516a39f2da4e872892
bfernando1 <bradleylasnames@gmail.com>
2021-01-30 18:28:45 -0800

    Add dynamic session log

--- week6/exercise4/exercise4b.py
+++ week6/exercise4/exercise4b.py
@@ -4,7 +4,15 @@ from nornir.plugins.functions.text import print_result
 from nornir.core.filter import F
 import os
 
+
+def set_session_name(task):
+    filename = f"{task.host}-output.log"
+    group_object = task.host.groups.refs[0]
+    group_object.connection_options["netmiko"].extras["session_log"] = filename
+
+
 def send_command(task):
+    task.run(task=set_session_name)
     cmd = "show feature | grep enabled"
     task.host.password = os.environ["BF_PASSWORD"]
     task.run(task=netmiko_send_command, command_string=cmd)
